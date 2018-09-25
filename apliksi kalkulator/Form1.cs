3906e6ba68c2b6f9969ae3b35ac9dc3b2854d70b
irfan prasetyoke <irfanprstyo@gmail.com>
2018-09-25 20:49:21 +0700

    Add project files.

---
+++ apliksi kalkulator/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apliksi_kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            hasil.ReadOnly = true;
+            operasiHitung.Items.Add("Penambahan");
+            operasiHitung.Items.Add("Pengurangan");
+            operasiHitung.Items.Add("Perkalian");
+            operasiHitung.Items.Add("Pembagian");
+            operasiHitung.SelectedItem = "Penambahan";
+        }
+        static int penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int pembagian(int a, int b)
+        {
+            return a / b;
+        }
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            var a = Convert.ToInt32(txtNilaiA.Text);
+            var b = Convert.ToInt32(txtNilaiB.Text);
+            string ambilOperasi = operasiHitung.Text;
+
+            if (ambilOperasi == "Penambahan")
+            {
+                hasil.Text = Convert.ToString(penambahan(a, b));
+            }
+            else if (ambilOperasi == "Pengurangan")
+            {
+                hasil.Text = Convert.ToString(pengurangan(a, b));
+
+            }
+            else if (ambilOperasi == "Perkalian")
+            {
+                hasil.Text = Convert.ToString(perkalian(a, b));
+
+            }
+            else
+            {
+                hasil.Text = Convert.ToString(pembagian(a, b));
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
